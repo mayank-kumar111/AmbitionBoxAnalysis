c17c76b18cca22f898490f5c1e66608d8d4a4aa9
Mayank Kumar <168926111+mayank-kumar111@users.noreply.github.com>
2026-08-27 16:45:56 +0530

    refactor(scraper): expose scraper public API

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,1 +1,6 @@
 """AmbitionBox data collection package."""
+
+from .ambitionbox_scraper import AmbitionBoxScraper
+from .config import ScraperConfig
+
+__all__ = ["AmbitionBoxScraper", "ScraperConfig"]
