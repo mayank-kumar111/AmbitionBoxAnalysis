962641f63ae7f5f97a2aeddefff655b1140703a3
Mayank Kumar <168926111+mayank-kumar111@users.noreply.github.com>
2026-08-27 17:01:00 +0530

    refactor(preprocessing): expose cleaning and quality APIs

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,12 +1,15 @@
-"""Data cleaning and preprocessing package."""
+"""Data cleaning, preprocessing, and data-quality utilities."""
 
-from .cleaner import clean_csv, clean_dataframe, parse_other_data
+from .cleaner import FINAL_COLUMNS, clean_csv, clean_dataframe, parse_other_data
+from .quality import profile_dataframe
 from .validator import validate_dataframe, validate_or_raise
 
 __all__ = [
+    "FINAL_COLUMNS",
     "clean_csv",
     "clean_dataframe",
     "parse_other_data",
+    "profile_dataframe",
     "validate_dataframe",
     "validate_or_raise",
 ]
